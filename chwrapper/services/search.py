fa4bdcb95ab080cbc5a91de0e196b0f154d0c50f
JamesGardiner <jamesg87@me.com>
2016-01-15 23:04:42 +0000

    remove extraneous merge dict function

--- chwrapper/services/search.py
+++ chwrapper/services/search.py
@@ -24,17 +24,6 @@
 from .base import Service
 
 
-def merge_dicts(*dict_args):
-    '''
-    Given any number of dicts, shallow copy and merge into a new dict,
-    precedence goes to key value pairs in latter dicts.
-    '''
-    result = {}
-    for dictionary in dict_args:
-        result.update(dictionary)
-    return result
-
-
 class CompanySearch(Service):
     """Search for companies by name.
     """
