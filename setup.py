efbd2929db38d069387c8a9c6409daa9b59e6f5a
James Gardiner <jamesg87@me.com>
2016-01-27 15:14:27 +0000

    Add requirements to setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from setuptools import setup
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
 setup(name='chwrapper',
       version='0.1',
       description='A simple wrapper around the Companies House API',
+      long_description=readme(),
       url='http://github.com/jamesgardiner/chwrapper',
       author='James Gardiner',
       author_email='jamesg87@me.com',
       license='MIT',
       packages=['chwrapper'],
-      zip_safe=False)
+      zip_safe=False,
+      install_requires=[
+          'requests==2.9.1',
+      ])
