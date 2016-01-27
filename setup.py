54a613d11189a8b9474e826f3250ab798752f7fd
James Gardiner <jamesg87@me.com>
2016-01-27 15:23:18 +0000

    Add classifiers to setup.py

--- setup.py
+++ setup.py
@@ -16,4 +16,15 @@ setup(name='chwrapper',
       zip_safe=False,
       install_requires=[
           'requests==2.9.1',
+      ],
+      classifiers=[
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
       ])
