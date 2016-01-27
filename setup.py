6366acc005766afaf548ec1d05f1d83948eb1a56
James Gardiner <jamesg87@me.com>
2016-01-27 14:42:03 +0000

    add setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='chwrapper',
+      version='0.1',
+      description='A simple wrapper around the Companies House API',
+      url='http://github.com/jamesgardiner/chwrapper',
+      author='James Gardiner',
+      author_email='jamesg87@me.com',
+      license='MIT',
+      packages=['chwrapper'],
+      zip_safe=False)
